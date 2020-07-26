e9d99b256db570010632e631432365e7b5640d39
Giovanni Gadaleta <61932706+gadaxmagicgadax@users.noreply.github.com>
2020-07-26 17:44:43 +0200

    Update settings.py
    eliminated secret key

--- graphql4iosdk/settings.py
+++ graphql4iosdk/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+z07+mdkjnn*=40144_kb%wr)rf!21o+zb%ykv2r@%)ojlcip+'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
